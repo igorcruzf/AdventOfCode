12795ee00103ca3eaf2eca5acf5369e7202eff47
igorcruzf <igfigueiredo@uolinc.com>
2021-12-01 18:22:07 -0300

    Using only one line

--- 1/main.py
+++ 1/main.py
@@ -1,7 +1,7 @@
-def one_line_part_one(lines=[int(line) for line in open("1/input.txt").readlines()]): print(sum(1 for i in range(len(lines)-1) if lines[i+1] > lines[i]))
+def one_line_part_one(lines=[int(line) for line in open("input.txt").readlines()]): print(sum(1 for i in range(len(lines)-1) if lines[i+1] > lines[i]))
 
 
-def one_line_part_two(lines=[int(line) for line in open("1/input.txt").readlines()]): print(sum(1 for i in range(len(lines)-3) if lines[i] + lines[i+1] + lines[i+2] < lines[i+1] + lines[i+2] + lines[i+3]))
+def one_line_part_two(lines=[int(line) for line in open("input.txt").readlines()]): print(sum(1 for i in range(len(lines)-3) if lines[i] + lines[i+1] + lines[i+2] < lines[i+1] + lines[i+2] + lines[i+3]))
 
 
 if __name__ == '__main__':
